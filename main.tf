20ce5c4e92042f8ac78e42d07697e41b2162a3f1
mjgwin <micahjgwin@gmail.com>
2023-02-16 14:26:30 -0600

    first commit, see if lambda deploys

--- main.tf
+++ main.tf
@@ -4,15 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.52.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
   }
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "tf-cloud-workspace"
 
     workspaces {
       name = "gh-actions-demo"
@@ -24,56 +20,34 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "random_pet" "sg" {}
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
+resource "aws_lambda_function" "github-actions-test-lambda" {
+  filename      = "lambda_code..zip"
+  function_name = "github-actions-test-lambda"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_code.github_actions_test_lambda.lambda_handler"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
+  runtime = "python3.9"
+
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
